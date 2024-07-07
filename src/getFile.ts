f8f58d250f839df2526e43ef864b922549945858
Isaac <105789412+altaskur@users.noreply.github.com>
2024-07-07 02:42:56 +0200

    feat: :sparkles: Validate args, source and file type

---
+++ src/getFile.ts
@@ -0,0 +1,25 @@
+import { readFileSync } from 'fs';
+import { checkFilePath, checkUrl } from './checkArgs';
+import { errorMessage, infoMessage } from './customMessages';
+
+const getSwagger = async (url: string): Promise<string > => {
+  infoMessage('Checking if is a local file...');
+
+  if (checkFilePath(url)) {
+    infoMessage('Reading the file...');
+    return readFileSync(url, 'utf8');
+  }
+  errorMessage('The file does not exist');
+
+  infoMessage('Checking if is a URL...');
+  const response = await checkUrl(url);
+  if (response) {
+    infoMessage('Reading the URL...');
+    const data = await fetch(url);
+    return data.text();
+  }
+
+  throw new Error('The file does not exist');
+};
+
+export default getSwagger;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,31 @@
+import { errorMessage, infoMessage } from './customMessages';
 import {
-  alertMessage, errorMessage, infoMessage, normalMessage, successMessage,
-} from './customMessages';
+  checkIfJson, checkIfYaml, checkUserArgs,
+} from './checkArgs';
 import showWelcomeMessage from './welcomeMessage';
+import getSwagger from './getFile';
 
 showWelcomeMessage();
+infoMessage('Checking the file...');
 
-errorMessage('This is an error message');
-alertMessage('This is an alert message');
-successMessage('This is a success message');
-infoMessage('This is an info message');
-normalMessage('This is a normal message');
+(async () => {
+  try {
+    const args = await checkUserArgs();
+    infoMessage('Getting the Swagger file...');
+
+    const swagger = await getSwagger(args);
+
+    const isJson = checkIfJson(swagger);
+    const isYaml = checkIfYaml(swagger);
+
+    if (!isJson && !isYaml) {
+      errorMessage('The file must be a JSON or YAML file');
+      process.exit(1);
+    }
+
+    infoMessage('The file is a JSON or YAML file');
+  } catch (error: unknown) {
+    errorMessage(error as string);
+    process.exit(1);
+  }
+})();
